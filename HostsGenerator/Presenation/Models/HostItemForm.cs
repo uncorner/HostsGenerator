ef241f97740ec69c6666b8d80fc165fa4493d7a5
uncorner <denis.uncorner@gmail.com>
2023-05-04 19:56:40 +0300

    use Display attr

--- HostsGenerator/Presenation/Models/HostItemForm.cs
+++ HostsGenerator/Presenation/Models/HostItemForm.cs
@@ -5,15 +5,18 @@ namespace HostsGenerator.Presenation.Models
 {
     public class HostItemForm
     {
+        [Display(Name = "Название")]
         [StringLength(50, ErrorMessage = "Слишком много символов")]
         public string? Name { get; set; }
 
+        [Display(Name = "Адрес url")]
         [Required(ErrorMessage = "Введите адрес URL")]
         [Url(ErrorMessage = "Некорректный адрес URL")]
         [StringLength(255, ErrorMessage = "Слишком много символов")]
         [Remote("CheckUrlExists", "HostItem", ErrorMessage = "Хост с таким Url адресом уже был добавлен")]
         public string Url { get; set; } = string.Empty;
 
+        [Display(Name = "Применяется")]
         public bool IsEnabled { get; set; } = true;
 
     }
